7ba03e3056580052cb701e70634195d62119be8d
ajanthanthurairatnam <ajanthan.kt@gmail.com>
2018-01-03 12:32:39 +1100

    init
    https://docs.microsoft.com/en-us/aspnet/signalr/overview/getting-started/introduction-to-signalr

---
+++ SignalRChatApp/ChatHub .cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalRChatApp
+{
+    public class ChatHub : Hub
+    {
+        public void Send1(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
